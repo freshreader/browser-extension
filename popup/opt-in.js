2c6cecb3636f9d059f0ea8ffd7ef919597f696e1
Maxime Vaillancourt <mv@maximevaillancourt.com>
2021-02-06 15:28:51 -0500

    v1.0.2: add consent step

---
+++ popup/opt-in.js
@@ -0,0 +1,26 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  // set up the appearance of the popup depending on the outcome of the opt-in
+  browser.storage.local.get("optIn", function (result) {
+    document.getElementById("opt-in-prompt").hidden = result.optIn;
+    document.getElementById("after-opt-in").hidden = !result.optIn;
+  });
+
+  document.getElementById("button-enable").addEventListener(
+    "click",
+    function () {
+      browser.storage.local.set({ "optIn": true, "optInShown": true });
+      document.getElementById("opt-in-prompt").hidden = true;
+      document.getElementById("after-opt-in").hidden = false;
+      window.close(); // may not work, but still attempt
+    });
+
+  document.getElementById("button-cancel").addEventListener(
+    "click",
+    function () {
+      browser.storage.local.set({ "optIn": false, "optInShown": true });
+      document.getElementById("opt-in-prompt").hidden = false;
+      document.getElementById("after-opt-in").hidden = true;
+    });
+});
--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,17 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  save_url = 'https://freshreader.app/save?url='+encodeURIComponent(tab.url)
-  chrome.tabs.update(tab.id, { url: save_url });
+chrome.browserAction.onClicked.addListener(function (tab) {
+  browser.storage.local.get("optIn", function (result) {
+    if (result.optIn) {
+      save_url = 'https://freshreader.app/save?url=' + encodeURIComponent(tab.url)
+      chrome.tabs.update(tab.id, { url: save_url });
+    } else {
+      browser.tabs.create({ url: "popup/opt-in.html" });
+    }
+  })
+});
+
+// show the tab if we haven't registered the user reacting to the prompt.
+browser.storage.local.get("optInShown", function (result) {
+  if (!result.optInShown) {
+    browser.tabs.create({ url: "popup/opt-in.html" });
+  }
 });
